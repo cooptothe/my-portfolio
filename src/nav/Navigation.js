37f27c50ee5603a1e497c8b27310dd4f45d1792e
cooptothe <edward.cooper97@gmail.com>
2023-10-11 18:39:42 -0500

    added icons to nav

--- src/nav/Navigation.js
+++ src/nav/Navigation.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import { Link, useLocation } from "react-router-dom";
 import eyeOfHorus from "../assets/eye-of-horus.png";
+import magicCircle from "../assets/magic-circle.png";
+import pyramids from "../assets/pyramids.png";
+import scroll from "../assets/scroll.png";
 import "../styles/nav.css";
 
 export default class Navigation extends Component {
@@ -8,6 +11,9 @@ export default class Navigation extends Component {
     return (
       <div>
         <img src={eyeOfHorus} />
+        <img src={magicCircle} />
+        <img src={pyramids} />
+        <img src={scroll} />
       </div>
     );
   }
