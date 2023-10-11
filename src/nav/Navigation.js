0347be616dcc75ce8e623286d27fa92b8d7866a6
cooptothe <edward.cooper97@gmail.com>
2023-10-11 18:29:39 -0500

    updates css backgroud

--- src/nav/Navigation.js
+++ src/nav/Navigation.js
@@ -1,9 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Link, useLocation } from "react-router-dom";
+import eyeOfHorus from "../assets/eye-of-horus.png";
+import "../styles/nav.css";
 
 export default class Navigation extends Component {
   render() {
     return (
-      <div>Navigation</div>
-    )
+      <div>
+        <img src={eyeOfHorus} />
+      </div>
+    );
   }
 }
