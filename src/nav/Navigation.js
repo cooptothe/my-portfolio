5fcf7a56358996edc7a26ccaee8e84c25875b2b7
cooptothe <edward.cooper97@gmail.com>
2023-10-11 19:05:00 -0500

    building nav functionality

--- src/nav/Navigation.js
+++ src/nav/Navigation.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React, { Component } from "react";
 import { Link, useLocation } from "react-router-dom";
 import eyeOfHorus from "../assets/eye-of-horus.png";
@@ -6,7 +7,41 @@ import pyramids from "../assets/pyramids.png";
 import scroll from "../assets/scroll.png";
 import "../styles/nav.css";
 
-export default class Navigation extends Component {
+export default Navigation() = () => {
+    const location = useLocation();
+    const getNavPosition = () => {
+        switch (location.pathname) {
+            case "/":
+                return "nav-about";
+            case "/skills":
+                return "nav-skills";
+            case "/projects":
+                return "nav-projects";
+            case "/contact":
+                return "nav-contact";
+            default:
+                return "";
+        }
+    };
+    const getTitle = () => {
+        switch (location.pathname) {
+            case "/":
+                return "ABOUT";
+            case "/skills":
+                return "SKILLS";
+            case "/projects":
+                return "PROJECTS";
+            case "/contact":
+                return "CONTACT";
+            default:
+                return "";
+        }
+    }
+    const navPosition = getNavPosition();
+    const pageTitle = getTitle();
+    const isCurrentPage = (navClass) => {
+        return navClass === navPosition;
+    }
   render() {
     return (
       <div>
