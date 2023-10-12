155f7e24c741154e7eebe230ca71ab2a932a5d88
cooptothe <edward.cooper97@gmail.com>
2023-10-11 19:47:59 -0500

    created rest of nav imports

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
 import Navigation from './nav/Navigation.js';
 import About from "./about/About";
+import Skills from "./skills/Skills";
+import Projects from "./projects/Projects";
+import Contact from "./contact/Contact";
 
 const App = () => {
   return  <Router>
   <Navigation />
   <Routes>
     <Route path="/" element={About} />
+    <Route path="/skills" element={Skills} />
+    <Route path="/projects" element={Projects} />
+    <Route path="/contact" element={Contact} />
   </Routes>
     </Router>;
 }
