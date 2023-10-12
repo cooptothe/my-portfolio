de0e5f8d509573e386c904725f060ce5caca2396
cooptothe <edward.cooper97@gmail.com>
2023-10-11 21:40:02 -0500

    Create PlayerStats.js

---
+++ src/playerStats/PlayerStats.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/playersStats.css";
+
+const PlayerStats = () => {
+    return (
+        <div id="playerStats">
+            <h1>EDWARD COOPER LEVEL 24</h1>
+            <div id="playerStats__lines">
+            <span id ="Player__lines__thick"></span>
+            <span id ="Player__lines__thin"></span>
+            <h2>Software Engineer</h2>
+            </div>
+        </div>
+    )
+}
