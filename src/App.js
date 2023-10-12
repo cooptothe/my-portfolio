e5d2586235c161a1262b6a176d1fcd982b60b45e
cooptothe <edward.cooper97@gmail.com>
2023-10-11 21:22:16 -0500

    created background css and added styling / fixed app.js components

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import About from "./about/About";
 import Skills from "./skills/Skills";
 import Projects from "./projects/Projects";
 import Contact from "./contact/Contact";
-import "./styles/app.css"
+import "./styles/app.css";
+import Background from './background/Background.js';
 
 const App = () => {
   return  <Router>
   <Navigation />
+  <Background />
   <Routes>
-    <Route path="/" element={About} />
-    <Route path="/skills" element={Skills} />
-    <Route path="/projects" element={Projects} />
-    <Route path="/contact" element={Contact} />
+    <Route path="/" element={<About />} />
+    <Route path="/skills" element={<Skills />} />
+    <Route path="/projects" element={<Projects />} />
+    <Route path="/contact" element={<Contact />} />
   </Routes>
     </Router>;
 }
