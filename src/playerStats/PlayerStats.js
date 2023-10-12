c51311316e80256aa115f395db4c3272cef81a29
cooptothe <edward.cooper97@gmail.com>
2023-10-11 21:45:06 -0500

    Update PlayerStats.js

--- src/playerStats/PlayerStats.js
+++ src/playerStats/PlayerStats.js
@@ -4,7 +4,7 @@ import "../styles/playerStats.css";
 const PlayerStats = () => {
     return (
         <div id="playerStats">
-            <h1>EDWARD COOPER LEVEL 24</h1>
+            <h1>EDWARD COOPER LEVEL 25</h1>
             <div id="playerStats__lines">
             <span id ="Player__lines__thick"></span>
             <span id ="Player__lines__thin"></span>
