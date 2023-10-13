1f07ad3a4bcbffecc55bfb698529a35e92b738b9
cooptothe <edward.cooper97@gmail.com>
2023-10-12 22:01:57 -0500

    Update subheadingData.js

--- src/about/subheadingData.js
+++ src/about/subheadingData.js
@@ -1,32 +1,85 @@
 const subheadingData = {
-    1: [
-        {
-            title: "About Me",
-            content: (
-                <>
-                    <p>
-                    Hey there! I'm Edward Cooper, a versatile Software Engineer with a knack for diving into tasks headfirst. 
-                    My journey kickstarted in the realm of education and customer service, where adaptability and precision were my strong suits. 
-                    Now, transitioning into tech, I'm on fire with a newfound passion for crafting innovative solutions to real-world challenges through code. 
-                    My mission? To blend my existing strengths with tech prowess, making a positive impact through seamless, user-focused software. 
-                    I've got the skills—JavaScript, TypeScript, React, Node.js, Express, MySQL—and the drive to thrive in this fast-paced realm. 
-                    Let's code a brighter tomorrow together! 🗝️
-                    </p>
-                </>
-            ),
-        },
-    ],
-    2: [
-        {
-            title: "Operation Spark 2022-2023",
-            content: (
-                <>
-                    <p>
-                    IBC Fundamentals of Javascript, Functional Programming, and Web Development Level 3
-                    </p>
-                    <span>Advanced Software Engineering Immersive Program</span>
-                </>
-            ),
-        },
-    ],
+  1: [
+    {
+      title: "About Me",
+      content: (
+        <>
+          <p>
+            Hey there! I'm Edward Cooper, a versatile Software Engineer with a
+            knack for diving into tasks headfirst. My journey kickstarted in the
+            realm of education and customer service, where adaptability and
+            precision were my strong suits. Now, transitioning into tech, I'm on
+            fire with a newfound passion for crafting innovative solutions to
+            real-world challenges through code. My mission? To blend my existing
+            strengths with tech prowess, making a positive impact through
+            seamless, user-focused software. I've got the skills—JavaScript,
+            TypeScript, React, Node.js, Express, MySQL—and the drive to thrive
+            in this fast-paced realm. Let's code a brighter tomorrow together!
+            🗝️
+          </p>
+        </>
+      ),
+    },
+  ],
+  2: [
+    {
+      title: "Operation Spark 2022-2023",
+      content: (
+        <>
+          <p>
+            IBC Fundamentals of Javascript, Functional Programming, and Web
+            Development Level 3
+          </p>
+          <span>Advanced Software Engineering Immersive Program</span>
+        </>
+      ),
+    },
+  ],
+  3: [
+    {
+      title: "Buckle, Leader",
+      content: (
+        <>
+          <p>
+            • Conducted workload assessments and devised new operational
+            processes that led to a 20% increase in productivity. • Delivered a
+            comprehensive training program for new hires, covering company
+            policies, systems, and customer service techniques.
+          </p>
+          <span>06/2022 - 12/2022</span>
+        </>
+      ),
+    },
+    {
+      title: "SweetGreen, Trainer/Shift Leader",
+      content: (
+        <>
+          <p>
+            • Led and coached the team in at least one of the core areas of the
+            restaurant (e.g., service, kitchen, online ordering). • Streamlined
+            daily operations, managing all aspects of kitchen, customer
+            experience, and online ordering during shifts. • Implemented
+            efficient processes that reduced customer wait time by 20% and
+            increased online order accuracy by 15%.
+          </p>
+          <span>07/2021 - 05/2022</span>
+        </>
+      ),
+    },
+    {
+      title:
+        "Young Audiences Charter School, Special Education Paraprofessional",
+      content: (
+        <>
+          <p>
+            • Evaluated children's individual learning needs and designed
+            tailored Individualized Educational Plans (IEPs). • Implemented
+            strategies that improved academic performance and increased student
+            engagement.
+          </p>
+          <span>08/2019 - 07/2021</span>
+        </>
+      ),
+    },
+  ],
 };
