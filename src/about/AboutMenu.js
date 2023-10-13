4232991de7eeacaf0b2532745a42e4da8a17f0d9
cooptothe <edward.cooper97@gmail.com>
2023-10-12 22:28:50 -0500

    adding icons

--- src/about/AboutMenu.js
+++ src/about/AboutMenu.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import AboutMenuItem from "./AboutMenuItem";
 import AboutSubheading from "./AboutSubheading";
 import subheadingData from "./subheadingData";
+import personalIcon from "../assets/stone.png"
 
 export default class AboutMenu extends Component {
   render() {
--- src/about/AboutSubheading.js
+++ src/about/AboutSubheading.js
@@ -1,9 +1,17 @@
-import React, { Component } from 'react'
+import React from "react";
+import classNames from "classnames";
+import "../styles/aboutMenu.css";
 
-export default class AboutSubheading extends Component {
-  render() {
-    return (
-      <div>AboutSubheading</div>
-    )
-  }
-}
+const AboutSubheading = ({ title, content, active, onClick, menuItem }) => {
+  const subContainerClass = `sub-container-${menuItem}`;
+  return (
+    <div
+      className={classNames(subContainerClass, { "active-subheading": active })}
+    >
+      <h3 onClick={onClick}>{title}</h3>
+      <div className="p-container">{content}</div>
+    </div>
+  );
+};
+
+export default AboutSubheading;
