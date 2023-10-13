07c64ae0306dcc3ee7bdcb244347c5ec731be480
cooptothe <edward.cooper97@gmail.com>
2023-10-12 21:19:55 -0500

    added content to data

---
+++ src/about/subheadingData.js
@@ -0,0 +1,19 @@
+const subheadingData = {
+    1: [
+        {
+            title: "About Me",
+            content: (
+                <>
+                    <p>
+                    Hey there! I'm Edward Cooper, a versatile Software Engineer with a knack for diving into tasks headfirst. 
+                    My journey kickstarted in the realm of education and customer service, where adaptability and precision were my strong suits. 
+                    Now, transitioning into tech, I'm on fire with a newfound passion for crafting innovative solutions to real-world challenges through code. 
+                    My mission? To blend my existing strengths with tech prowess, making a positive impact through seamless, user-focused software. 
+                    I've got the skills—JavaScript, TypeScript, React, Node.js, Express, MySQL—and the drive to thrive in this fast-paced realm. 
+                    Let's code a brighter tomorrow together! 🗝️
+                    </p>
+                </>
+            ),
+        },
+    ],
+};
