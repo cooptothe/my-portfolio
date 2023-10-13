9f18d02adb4b77f886f3758bf98a7d3c3b35b763
cooptothe <edward.cooper97@gmail.com>
2023-10-12 22:32:15 -0500

    updated icon paths

--- src/about/AboutMenu.js
+++ src/about/AboutMenu.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import AboutMenuItem from "./AboutMenuItem";
 import AboutSubheading from "./AboutSubheading";
 import subheadingData from "./subheadingData";
-import personalIcon from "../assets/stone.png"
+import personalIcon from "../assets/stone.png";
+import educationIcon from "../assets/upgrade.png";
+import careerIcon from "../assets/triple-corn.png";
 
 export default class AboutMenu extends Component {
   render() {
