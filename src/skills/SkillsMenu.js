6b2331f1da35da92fbbeabe43d1075b4a248dd55
cooptothe <edward.cooper97@gmail.com>
2023-10-20 14:53:39 -0500

    started working on the skills menu

--- src/skills/SkillsMenu.js
+++ src/skills/SkillsMenu.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import classNames from 'classnames';
+import "../styles/skillsMenu.css";
+import skills from "./skillsData.js";
+import frontendIcon from "../assets/serpent-emblem.png";
+import backendIcon from "../assets/hermes-emblem.png";
 
 export default class SkillsMenu extends Component {
   render() {
