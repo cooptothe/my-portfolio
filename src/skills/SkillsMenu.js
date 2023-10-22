3a3d629e5a1d7c4f09037f635bc523e1cfd53b8a
cooptothe <edward.cooper97@gmail.com>
2023-10-22 16:08:45 -0500

    added skills data for skills menu

--- src/skills/SkillsMenu.js
+++ src/skills/SkillsMenu.js
@@ -1,14 +1,41 @@
-import React, { Component } from 'react';
-import classNames from 'classnames';
+import React, { Component } from "react";
+import classNames from "classnames";
 import "../styles/skillsMenu.css";
 import skills from "./skillsData.js";
 import frontendIcon from "../assets/serpent-emblem.png";
 import backendIcon from "../assets/hermes-emblem.png";
 
 export default class SkillsMenu extends Component {
-  render() {
-    return (
-      <div>SkillsMenu</div>
-    )
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeMenuItem: 1,
+    };
+  }
+
+  handleMenuItemClick = (menuItem) => {
+    this.setState({
+      activeMenuItem: menuItem,
+      activeSubheading: 1,
+    });
+  };
+
+  renderContent(skills) {
+    return skills.map((skill, index) => (
+      <div
+        key={index}
+        className={`skill-sub-container-${this.state.activeMenuItem}`}
+      >
+        <h3>{skill.title}</h3>
+        <div className="level-container">
+        {[...Array(6)].map((_, i) => (
+          <div
+            key={i}
+            className={`level-point ${i < skill.level ? "filled" : "unfilled"}`}
+          ></div>
+        ))}
+        </div>
+      </div>
+    ));
   }
 }
---
+++ src/skills/skillsData.js
@@ -0,0 +1,70 @@
+const skills = {
+    1: [
+        {
+            title: "HTML",
+            level: 6,
+        },
+        {
+            title: "CSS",
+            level: 5,
+        },
+        {
+            title: "SASS",
+            level: 2,
+        },
+        {
+            title: "JavaScript",
+            level: 5,
+        },
+        {
+            title: "TypeScript",
+            level: 4,
+        },
+        {
+            title: "Tailwind CSS",
+            level: 4,
+        },
+        {
+            title: "React",
+            level: 4,
+        },
+        {
+            title: "Figma",
+            level: 3,
+        },
+    ],
+    2: [
+        {
+            title: "HTML",
+            level: 6,
+        },
+        {
+            title: "MySQL",
+            level: 4,
+        },
+        {
+            title: "Node.js",
+            level: 4,
+        },
+        {
+            title: "MongoDB",
+            level: 2,
+        },
+        {
+            title: "Git",
+            level: 4,
+        },
+        {
+            title: "PHP",
+            level: 1,
+        },
+        {
+            title: "SQL",
+            level: 3,
+        },
+        {
+            title: "Express",
+            level: 3,
+        },
+    ]
+};
