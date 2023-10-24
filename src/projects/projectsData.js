4a4195c7806970e86c39d4bdf7b5ae861ca331f8
cooptothe <edward.cooper97@gmail.com>
2023-10-24 01:30:15 -0500

    done with projects page

--- src/projects/projectsData.js
+++ src/projects/projectsData.js
@@ -5,23 +5,12 @@ import projectThree from "../assets/project-3.png";
 const projects = {
   1: {
     title: "The Spot",
-    image: projectOne,
+    image: projectTwo,
     description: (
       <>
         <p>
           an app that makes it easier to find friends on a night out, by
-          displaying a map filled with users, live events, and venues •
-          Developed a dynamic sign-up page using React and implemented a
-          responsive image slider with auto-play functionality using Swiper
-          library to showcase app features. • Integrated Google Maps API for
-          location suggestions and utilized the Geolocation API for precise user
-          positioning. • Implemented a user location pin using Google Maps API,
-          and integrated socket.io for geolocation updates and real-time
-          location tracking. • Developed a dynamic user profile set-up/settings
-          page using Material-UI components and Redux for state management,
-          enabling users to update profile information, select map icons,
-          customize font sizes, and securely upload profile pictures. •
-          Integrated speech-to-text functionality for improved accessibility.
+          displaying a map filled with users, live events, and venues
         </p>
       </>
     ),
@@ -29,21 +18,12 @@ const projects = {
   },
   2: {
     title: "Thriller",
-    image: projectTwo,
+    image: projectOne,
     description: (
       <>
         <p>
           a mobile application for horror movie enthusiasts that allows users to
-          discover trending, upcoming, top-rated, and classic horror movies •
-          Implemented UI/UX design and integrated various functionalities using
-          React Native. • Utilized Expo tools for seamless development and
-          deployment of the mobile application. • Integrated Moviedb API to
-          fetch and display movie information, enhancing user experience. •
-          Developed features for viewing trending, upcoming, top-rated, and
-          classic horror movies. • Implemented search functionality for users to
-          search movies by title. • Designed and developed the movie details
-          feature, providing in-depth information about specific movies,
-          including cast and similar movies.
+          discover trending, upcoming, top-rated, and classic horror movies
         </p>
       </>
     ),
@@ -57,18 +37,7 @@ const projects = {
       <>
         <p>
           an innovative conflict resolution app designed to help users handle
-          interpersonal conflicts effectively • Deployed the application to a
-          production environment by setting up and configuring an Amazon EC2
-          instance. • Implemented Google OAuth 2.0 authentication for the
-          application using Passport.js and passport-google-oauth20, enhancing
-          user security and enabling seamless sign-in with Google accounts. •
-          Developed a user profile component in React using Bootstrap,
-          efficiently managed state updates with Redux and facilitated API
-          communication with Axios. • Implemented multer storage for seamless
-          image uploads, enhancing the user experience and profile customization
-          capabilities. • Designed a comprehensive schema for user data using
-          MySQL in conjunction with Sequelize, ensuring efficient data
-          management and database integrity.
+          interpersonal conflicts effectively
         </p>
       </>
     ),
